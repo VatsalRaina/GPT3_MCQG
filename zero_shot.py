8c040bbbfda8af16ef80ba033e6a4e8b86a7a43d
Vatsal Raina <vatsalraina@hotmail.co.uk>
2022-08-03 22:30:57 +0100

    zero shot

---
+++ zero_shot.py
@@ -0,0 +1,53 @@
+import argparse
+import os
+import sys
+import openai
+
+parser = argparse.ArgumentParser(description='Get all command line arguments.')
+parser.add_argument('--prediction_save_path', type=str, help='Load path to which trained model will be saved')
+parser.add_argument('--context_path', type=str, help='Load path to contexts.txt')
+parser.add_argument('--openai_access_key', type=str, help='Access key from OpenAI')
+
+def main(args):
+    if not os.path.isdir('CMDs'):
+        os.mkdir('CMDs')
+    with open('CMDs/train.cmd', 'a') as f:
+        f.write(' '.join(sys.argv) + '\n')
+        f.write('--------------------------------\n')
+
+    with open(args.context_path, 'r') as f:
+        all_contexts = [a.rstrip() for a in f.readlines()]
+
+    all_responses = []
+
+    count = 0
+
+    for context in all_contexts:
+        response = openai.Completion.create(
+        model="text-davinci-002",
+        prompt="Generate a multiple-choice question with 4 options and answer for this passage\n\n"+context,
+        temperature=0.4,
+        max_tokens=3475,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+        )
+
+        response = response.replace("\n", " [SEP] ")
+
+        all_responses.append(response)
+
+        count += 1
+        if count == 3:
+            break
+
+    with open(args.save_path+"responses.txt", 'w') as f:
+        f.writelines("%s\n" % resp for resp in all_responses)
+
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args)
+
+
